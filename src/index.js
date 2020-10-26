0294d38eb5b509bddd018d709bb95d2307cfaeb9
dennywahyudimarzuk <dennywm66@gmail.com>
2020-10-26 10:48:05 +0700

    test push

--- src/index.js
+++ src/index.js
@@ -20,3 +20,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+//tes push
